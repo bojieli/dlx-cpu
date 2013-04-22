c2af8367801890cf208f8238ab24fa486b27d775
boj <bojieli@gmail.com>
2013-04-22 20:29:33 +0800

    add constants to common.v

--- src/common.v
+++ src/common.v
@@ -1,5 +1,7 @@
 
-parameter REG_ADDR_SIZE = 5;
-parameter REG_NUM       = (1<<REG_ADDR_SIZE);
-parameter WORD_SIZE     = 32;
+parameter REG_ADDR_SIZE   = 5;
+parameter REG_NUM         = (1<<REG_ADDR_SIZE);
+parameter WORD_SIZE       = 32;
+parameter INST_QUEUE_SIZE = 8;
+parameter ALU_NUM         = 2;
 
